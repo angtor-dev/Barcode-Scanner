e346aee354a3dc6770facfe5d9b89bd25d24c455
Angel Torres <angtor.dev@gmail.com>
2022-12-09 10:02:21 -0400

    Condicional para acceder a getUserMedia

--- app.js
+++ app.js
@@ -1,30 +1,35 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const resultadoEl = document.getElementById("resultado")
+    // safely access `navigator.mediaDevices.getUserMedia`
+    if (navigator.mediaDevices && typeof navigator.mediaDevices.getUserMedia === 'function') {
+        const resultadoEl = document.getElementById("resultado")
 
-    Quagga.init({
-		inputStream: {
-			constraints: {
-				width: 1920,
-				height: 1080,
-			},
-			name: "Live",
-			type: "LiveStream",
-			target: document.getElementById("contenedor"),
-		},
-		decoder: {
-			readers: ["ean_reader"]
-		}
-	}, function (err) {
-		if (err) {
-			console.log(err);
-			return
-		}
-		console.log("Iniciado correctamente");
-		Quagga.start();
-	});
+        Quagga.init({
+	    	inputStream: {
+	    		constraints: {
+	    			width: 1920,
+	    			height: 1080,
+	    		},
+	    		name: "Live",
+	    		type: "LiveStream",
+	    		target: document.getElementById("contenedor"),
+	    	},
+	    	decoder: {
+	    		readers: ["ean_reader"]
+	    	}
+	    }, function (err) {
+	    	if (err) {
+	    		console.log(err);
+	    		return
+	    	}
+	    	console.log("Iniciado correctamente");
+	    	Quagga.start();
+	    });
 
-    Quagga.onDetected((data) => {
-		$resultados.textContent = data.codeResult.code;
-		console.log(data);
-	});
+        Quagga.onDetected((data) => {
+	    	$resultados.textContent = data.codeResult.code;
+	    	console.log(data);
+	    });
+    } else {
+        alert("No se puede acceder a los datos de la camara")
+    }
 })
