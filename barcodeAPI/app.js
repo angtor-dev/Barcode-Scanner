abcc493d14d6bd331d9d286ddfa4fc7a7db8cc7a
Angel Torres <angtor.dev@gmail.com>
2022-12-12 17:02:49 -0400

    Uso de la camara en vez de imagen

--- barcodeAPI/app.js
+++ barcodeAPI/app.js
@@ -1,3 +1,36 @@
+// solicita permisos para usar camara y sus ajustes por defecto
+navigator.mediaDevices.getUserMedia({
+    video: {
+        width: {min: 480, ideal: 1080},
+        height: {min: 480, ideal: 1080},
+        aspectRatio: {ideal: 1},
+        facingMode: 'environment',
+    }
+})
+.then((stream) => {
+    const video = document.querySelector('video')
+    video.srcObject = stream
+
+    // get the active track of the stream
+    const track = stream.getVideoTracks()[0]
+    // console.log(track.getSettings())
+
+    // get current camera capabilities
+    let capabilities = track.getCapabilities()
+    console.log(capabilities)
+
+    // update a capability
+    if (capabilities.brightness) {
+        track.applyConstraints({
+            advanced: [{
+                brightness: 0
+            }]
+        })
+        .catch(error => console.error("Error trying to apply constraints: ", error))
+    }
+})
+.catch(err => console.error('getUserMedia() failed: ', err))
+
 document.addEventListener("DOMContentLoaded", () => {
     if (!("BarcodeDetector" in window)) {
         alert("Este navegador no es compatible con la API BarcodeDetector")
@@ -5,15 +38,15 @@ document.addEventListener("DOMContentLoaded", () => {
     }
     
     const barcodeDetector = new BarcodeDetector()
-    const testImage = document.querySelector('img')
+    const video = document.querySelector('video')
 
     barcodeDetector
-    .detect(testImage)
+    .detect(video)
     .then((barcodes) => {
         let pre = document.createElement("pre")
         pre.innerHTML = JSON.stringify(barcodes, null, 2)
 
-        testImage.after(pre)
+        video.after(pre)
     })
     .catch(console.error)
 })
