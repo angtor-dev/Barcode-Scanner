743c8610ab2fd03dbdcda99b8cc4e0bebffde3c5
Angel Torres <angtor.dev@gmail.com>
2022-12-13 10:56:02 -0400

    fix

--- barcodeAPI/app.js
+++ barcodeAPI/app.js
@@ -58,9 +58,9 @@ async function decode() {
         decoding = false
 
         console.log(barcodes)
-        console.log(barcodes[0].rawValue)
 
         if (barcodes.lenght != 0) {
+            console.log(barcodes[0].rawValue)
             document.getElementById("codigo").textContent = barcodes[0].rawValue
             document.getElementById("formato").textContent = barcodes[0].format
         }
