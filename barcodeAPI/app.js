1b6918f3efb7537135c9591b1bfc39a8de12ed81
Angel Torres <angtor.dev@gmail.com>
2022-12-13 09:08:32 -0400

    prueba con funcion asincrona

--- barcodeAPI/app.js
+++ barcodeAPI/app.js
@@ -1,60 +1,65 @@
-// solicita permisos para usar camara y sus ajustes por defecto
-navigator.mediaDevices.getUserMedia({
-    video: {
-        width: {min: 480, ideal: 1080},
-        height: {min: 480, ideal: 1080},
-        aspectRatio: {ideal: 1},
-        facingMode: 'environment',
-    }
-})
-.then((stream) => {
-    const video = document.querySelector('video')
-    video.srcObject = stream
+const videoEl = document.getElementById("camara")
+videoEl.addEventListener("loadeddata", onOpened)
 
-    // get the active track of the stream
-    const track = stream.getVideoTracks()[0]
-    // console.log(track.getSettings())
+var decodeInterval
+var decoding = false
 
-    // get current camera capabilities
-    let capabilities = track.getCapabilities()
-    console.log(capabilities)
-
-    // update a capability
-    if (capabilities.brightness) {
-        track.applyConstraints({
-            advanced: [{
-                brightness: 0
-            }]
-        })
-        .catch(error => console.error("Error trying to apply constraints: ", error))
-    }
-})
-.catch(err => console.error('getUserMedia() failed: ', err))
-
-document.addEventListener("DOMContentLoaded", () => {
-    if (!("BarcodeDetector" in window)) {
-        alert("Este navegador no es compatible con la API BarcodeDetector")
-        return
-    }
-    
+if (!("BarcodeDetector" in window)) {
+    alert("Este navegador no es compatible con la API BarcodeDetector")
+} else {
     const barcodeDetector = new BarcodeDetector()
-    const video = document.querySelector('video')
 
-    var barcodes = await barcodeDetector.detect(video);
+    // solicita permisos para usar camara y sus ajustes por defecto
+    navigator.mediaDevices.getUserMedia({
+        video: {
+            width: {min: 480, ideal: 1080},
+            height: {min: 480, ideal: 1080},
+            aspectRatio: {ideal: 1},
+            facingMode: 'environment',
+        }
+    })
+    .then((stream) => {
+        videoEl.srcObject = stream
 
-    alert("Codigo encontrado" + barcodes.rawValue)
+        // get the active track of the stream
+        const track = stream.getVideoTracks()[0]
+        // console.log(track.getSettings())
 
+        // get current camera capabilities
+        let capabilities = track.getCapabilities()
+        console.log(capabilities)
 
+        // update a capability
+        // if (capabilities.brightness) {
+        //     track.applyConstraints({
+        //         advanced: [{
+        //             brightness: 0
+        //         }]
+        //     })
+        //     .catch(error => console.error("Error trying to apply constraints: ", error))
+        // }
+    })
+    .catch(err => console.error('getUserMedia() failed: ', err))
 
-    // setInterval(() => {
-    //     barcodeDetector
-    //     .detect(video)
-    //     .then((barcodes) => {
-    //         let pre = document.createElement("pre")
-    //         pre.innerHTML = JSON.stringify(barcodes, null, 2)
-    
-    //         video.after(pre)
-    //     })
-    //     .catch(console.error)
-    // }, 100)
-})
+    function onOpened() {
+        clearInterval(decodeInterval)
+        decodeInterval = setInterval(decode, 500)
+    }
+
+    async function decode() {
+        if (decoding === false) {
+            console.log("Decoding...")
+
+            decoding = true
+            let barcodes = await barcodeDetector.detect(videoEl)
+            decoding = false
+
+            document.getElementById("codigo").textContent(barcodes.rawValue)
+
+            let pre = document.createElement("pre")
+            pre.innerHTML = JSON.stringify(barcodes, null, 2)
+
+            videoEl.after(pre)
+        }
+    }
+}
